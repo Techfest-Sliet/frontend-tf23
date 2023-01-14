aa17a977bdd06a472d1acb2722195d23f58c1d45
Shubham Kumar Singh <95537249+theAlphaCoder06@users.noreply.github.com>
2023-01-14 22:56:34 +0530

    Signin, Reset Password, Forgot Password & Error-404

---
+++ src/components/Error404/Error404.js
@@ -0,0 +1,21 @@
+import "./Error404.css";
+import logo from "../../images/techFEST '23.webp";
+
+export default function error404() {
+  return (
+    <>
+      <div>
+        <img src={logo} alt="techFestSLIET'23 logo"  className="error404__bg"/>
+      </div>
+      <div className="error404__content">
+        <h1 className="error404__title">Error 404!</h1>
+        <div className="error404__text">
+          <span>Oops! Lost your way?</span>
+          <br />
+          Sorry, we can't find the page you are looking for.
+        </div>
+        <button className="error404__button">Home</button>
+      </div>
+    </>
+  );
+}
