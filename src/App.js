489ec3374c799f6e94f01a38da373405bbaf1cef
Sandeep Kumar <sandeepqmar05@gmail.com>
2023-01-18 16:32:20 +0530

    cleanup after merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import HomeScreen from './screens/landingPage/HomeScreen';
+import Reset from './components/Signin/Signin'
 import './index.css';
 import { Route, Routes } from 'react-router';
 function App() {
