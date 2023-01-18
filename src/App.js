6c9a3c1ac05d36856075f9bae6ba1412fe53f05f
Sandeep Kumar <sandeepqmar05@gmail.com>
2023-01-18 21:02:21 +0530

    Cleanup after merging ContactUs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 
-// import ContactUs from "./components/contactUs";
-// import Workshops from "./components/workshop/workshops"
-// import DataOfWorkshop from "./components/workshop/DataOfWorkshop";
-import Loading from "./components/loading/Loading";
+import ContactUs from "./components/contactUs";
 import React from 'react';
 import HomeScreen from './screens/landingPage/HomeScreen';
 import Reset from './components/Signin/Signin'
@@ -12,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Loading/>}/>
-        <Route path='/home' element={<HomeScreen/>}/>
+        <Route path='/contactUs' element={<ContactUs/>}/>
+        <Route path='/' element={<HomeScreen/>}/>
         <Route path='/signIn' element={<Reset/>}/>
       </Routes>
     </div>
