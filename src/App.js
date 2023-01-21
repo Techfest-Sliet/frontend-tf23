a0dd4b589f756f60e61bb9cd208baaa9b1339918
Sandeep Kumar <sandeepqmar05@gmail.com>
2023-01-21 23:55:02 +0530

    Removed App.css and logo.png

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
+import { Navbar } from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Rough from './components/rough';
 
-// import ContactUs from "./components/contactUs";
-// import Workshops from "./components/workshop/workshops"
-// import DataOfWorkshop from "./components/workshop/DataOfWorkshop";
-import Loading from "./components/loading/Loading";
-import React from 'react';
-import HomeScreen from './screens/landingPage/HomeScreen';
-import Reset from './components/Signin/Signin'
-import './index.css';
-import { Route, Routes } from 'react-router';
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Loading/>}/>
-        <Route path='/home' element={<HomeScreen/>}/>
-        <Route path='/signIn' element={<Reset/>}/>
-      </Routes>
-    </div>
+  <>
+  <Navbar/>
+  <Rough/>
+  <Footer/>
+  </>
   );
 }
 
