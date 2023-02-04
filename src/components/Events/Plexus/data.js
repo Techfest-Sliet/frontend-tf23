4698d28dfb7ed9f374a135780a73a529a639b62d
Varsha <varsha20046kumari@gmail.com>
2023-02-04 17:31:31 +0530

    chng in domain coordinator

--- src/components/Events/Plexus/data.js
+++ src/components/Events/Plexus/data.js
@@ -1,27 +1,5 @@
 export const datasOfEvent = [
     {
-        nameOfEvent: "Code Sprint",
-        desc: "Heard Competitive Programmers cracking Top-notch Companies. Here's the secret. They've been analyzing themselves thoroughly.Participate in our event Codesprint and push yourself to get ahead in your programming skills.",
-        register: (<span><a rel='noopener noreferrer' href='https://www.wikipedia.com/' style={{ textDecoration: "none", color: "white" }}>Register Now</a></span>),
-        schedule: (<span><a rel='noopener noreferrer' href='https://www.google.com/' style={{ textDecoration: "none", color: "white" }}>Problem Statement</a></span>),
-        prizeMoney: "₹ 12,000",
-        dateBefore: "15/03",
-        coor: [{
-            img: require('../../dummy.jpg'),
-            nameOfEventCoordinator: "Shubham Singh",
-            phoneNo: (<span><a href="tel:9506846608" style={{ textDecoration: "none", color: "white" }}> 9506846608 </a></span>),
-            mail: (<span><a href='mailto:2140103@sliet.ac.in' style={{ textDecoration: "none", color: "white" }}> 2140103@sliet.ac.in
-            </a></span>)
-        },
-        {
-            img: require('../../dummy.jpg'),
-            nameOfEventCoordinator: "Chitresh Kumar Gupta",
-            phoneNo: (<span><a href="tel:8303415894" style={{ textDecoration: "none", color: "white" }}> 8303415894 </a></span>),
-            mail: (<span><a href='mailto: 2140108@sliet.ac.in' style={{ textDecoration: "none", color: "white" }}> 2140108@sliet.ac.in
-            </a></span>)
-        }]
-    },
-    {
         nameOfEvent: "Data dynamics",
         desc: "It's time to get dynamic with your Data Science knowledge. Use your programming knowledge and combine it with statistical knowledge to get insights from data. Participate in our event Data Dynamic and win worth cash prizes.",
         register: (<span><a rel='noopener noreferrer' href='https://www.wikipedia.com/' style={{ textDecoration: "none", color: "white" }}>Register Now</a></span>),
@@ -45,7 +23,7 @@ export const datasOfEvent = [
             }]
     },
     {
-        nameOfEvent: "Techn IQ",
+        nameOfEvent: "Tech-n-IQ",
         desc: "Though Cheetah is the fastest, horse runs no slow. Here's to the event creating a healthy competition between best brains yet differing in speed.Set your mettle high and participate in our event to win handsome cash prizes",
         register: (<span><a rel='noopener noreferrer' href='https://www.wikipedia.com/' style={{ textDecoration: "none", color: "white" }}>Register Now</a></span>),
         schedule: (<span><a rel='noopener noreferrer' href='https://www.google.com/' style={{ textDecoration: "none", color: "white" }}>Problem Statement</a></span>),
@@ -85,6 +63,27 @@ export const datasOfEvent = [
             nameOfEventCoordinator: "Amartya Kumar",
             phoneNo: (<span><a href="tel:6203729451" style={{ textDecoration: "none", color: "white" }}> 6203729451 </a></span>),
             mail: (<span><a href='mailto: 2130458@sliet.ac.in' style={{ textDecoration: "none", color: "white" }}> 2130458@sliet.ac.in
+            </a></span>)
+        }]
+    }, {
+        nameOfEvent: "Code Sprint",
+        desc: "Heard Competitive Programmers cracking Top-notch Companies. Here's the secret. They've been analyzing themselves thoroughly.Participate in our event Codesprint and push yourself to get ahead in your programming skills.",
+        register: (<span><a rel='noopener noreferrer' href='https://www.wikipedia.com/' style={{ textDecoration: "none", color: "white" }}>Register Now</a></span>),
+        schedule: (<span><a rel='noopener noreferrer' href='https://www.google.com/' style={{ textDecoration: "none", color: "white" }}>Problem Statement</a></span>),
+        prizeMoney: "₹ 12,000",
+        dateBefore: "15/03",
+        coor: [{
+            img: require('../../dummy.jpg'),
+            nameOfEventCoordinator: "Shubham Singh",
+            phoneNo: (<span><a href="tel:9506846608" style={{ textDecoration: "none", color: "white" }}> 9506846608 </a></span>),
+            mail: (<span><a href='mailto:2140103@sliet.ac.in' style={{ textDecoration: "none", color: "white" }}> 2140103@sliet.ac.in
+            </a></span>)
+        },
+        {
+            img: require('../../dummy.jpg'),
+            nameOfEventCoordinator: "Chitresh Kumar Gupta",
+            phoneNo: (<span><a href="tel:8303415894" style={{ textDecoration: "none", color: "white" }}> 8303415894 </a></span>),
+            mail: (<span><a href='mailto: 2140108@sliet.ac.in' style={{ textDecoration: "none", color: "white" }}> 2140108@sliet.ac.in
             </a></span>)
         }]
     }
