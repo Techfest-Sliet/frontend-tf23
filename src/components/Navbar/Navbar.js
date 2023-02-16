1bde700d12cd3b90b88f4ea571932c9c131f69df
Ashish Kumar <ashish13122000@gmail.com>
2023-02-16 19:24:40 +0530

    improved design and added dropdown in about us

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -69,3 +69,108 @@ export const Navbar = (props) => {
   )
 }
 
+<<<<<<< Updated upstream
+=======
+          <ul className="navbar-items">
+            <li>
+              <Link to="/" onClick={unCheck}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/under-construction" onClick={unCheck}>
+                Workshops
+              </Link>
+            </li>
+            <li className="aboutUs">
+              <Link to="/about" className="dropdown__animation" onClick={unCheck}>
+                About Us<i className="arrow down">&#32;</i>
+              </Link>
+              <ul className="dropdown">
+                <li>
+                  <Link to="/faq" onClick={unCheck}>
+                    FAQ
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/our-team" onClick={unCheck}>
+                    Our Team
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            {authContext.isUserLoggedIn && (
+              <li>
+                <Link to="/user-dashboard">Dashboard</Link>
+              </li>
+            )}
+            <li className="domains">
+              <Link
+                to="/domains"
+                className="dropdown__animation"
+                onClick={unCheck}
+              >
+                Domains<i className="arrow down">&#32;</i>
+              </Link>
+              {/*-- DROPDOWN MENU */}
+              <ul className="dropdown">
+                <li>
+                  <Link to="/events/plexus" onClick={unCheck}>
+                    Plexus
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/events/chemfor" onClick={unCheck}>
+                    Chemfor
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/events/electrica" onClick={unCheck}>
+                    Electrica
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/events/genesis" onClick={unCheck}>
+                    Genesis
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/events/karyarachna" onClick={unCheck}>
+                    Karyarachna
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/events/kermis" onClick={unCheck}>
+                    Kermis
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/events/mechanica" onClick={unCheck}>
+                    Mechanica
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/events/robozar" onClick={unCheck}>
+                    Robozar
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            {!authContext.isUserLoggedIn && (
+              <Link to="/sign-in" className="signInButton">
+                Sign In
+              </Link>
+            )}
+            {authContext.isUserLoggedIn && (
+              <Link to="/" className="signInButton" onClick={logOutHandler}>
+                Logout
+              </Link>
+            )}
+          </ul>
+        </div>
+      </header>
+    </navbar>
+  );
+};
+>>>>>>> Stashed changes
