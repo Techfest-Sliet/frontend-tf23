fb071bbe59e13fca7cec7fab6d42262061d1fc27
ghshivamkashyap <2130103@sliet.ac.in>
2023-02-18 00:15:28 +0530

    completed feedback page v1

--- src/components/Feedback/feedback.jsx
+++ src/components/Feedback/feedback.jsx
@@ -1,3 +1,4 @@
+import { Email } from '@mui/icons-material'
 import React from 'react'
 import './feedback.css'
 
@@ -5,15 +6,21 @@ const Feedback = () => {
   return (
    
 <div className='container'>
-      <div className='feedbackdiv'>
-        <div className='gradientimageright'>
-        <div className='gradientimageleft'>
-        </div>
-        </div>
-        <div className='techfestimg'></div>
-        {/* <div className='gradientimageleft'></div> */}
-        {/* <div className='techfestimg' ></div> */}
-      </div>
+     <div className='feedbacktfimage'>
+     
+     <form className='feedbackform'>
+     <p className='feedbackp'>Write Us!</p>
+      <input placeholder='Enter Your Name'  className='feedbackinput1'></input>
+      <input type={Email} placeholder='Enter Your E-mail Address'  className='feedbackinput2'></input>
+      <br></br>
+      <input placeholder='Enter Your Phone Number'  className='feedbackinput3'></input>
+      <br></br>
+      <textarea className='feedbacktextarea' rows="10" cols="65">Submit your feedback.</textarea>
+      <br></br>
+      <div className='feedbacksubmitbtn'>Submit</div>
+    </form>
+
+     </div>
     </div>
   )
 }
