5d051a3d4946b3d2bb60b8320dd0872270555d09
Shubham Kumar Singh <95537249+theAlpha06@users.noreply.github.com>
2023-02-24 20:21:09 +0530

    Fixed image

--- src/components/merchandise/data.js
+++ src/components/merchandise/data.js
@@ -2,13 +2,13 @@
 export const data = [
   {
     id: 1,
-    mainImage: "./images/2.png",
-    thumbnail: "./images/2.png",
+    mainImage: "./images/1.png",
+    thumbnail: "./images/1.png",
   },
   {
     id: 2,
-    mainImage: "./images/1.png",
-    thumbnail: "./images/1.png",
+    mainImage: "./images/2.png",
+    thumbnail: "./images/2.png",
   },
   {
     id: 3,
