b0a6b882a9c741adc47479f0b2f1106dd12546fb
Sandeep Kumar <sandeepqmar05@gmail.com>
2023-03-17 12:17:09 +0530

    Connected payment frontend with backend

--- src/components/PaymentPage/paymentPage.jsx
+++ src/components/PaymentPage/paymentPage.jsx
@@ -1,23 +1,99 @@
 import React from "react";
 import "./paymentPage.css";
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../../auth/authContext";
+import { baseUrl } from "../../API/api";
+import axios from "axios";
 
 const Transaction = () => {
+  const authContext = useContext(AuthContext);
+  const [amount, setAmount] = useState("");
+  const [paymentId, setPaymentId] = useState("");
+  const [screenshot, setScreenshot] = useState(null);
+  const [user, setUser] = useState(null);
+  const HandleSubmit = () => {
+    console.log(screenshot);
+    const paymentData = new FormData();
+    paymentData.append("userId", user._id);
+    paymentData.append("paymentId", paymentId);
+    paymentData.append("paymentAmount", amount);
+    paymentData.append("paymentPhoto", screenshot);
+    axios.post(`${baseUrl}/payment/submit`, paymentData, {
+      headers: {
+        Authorization: "Bearer " + authContext.token,
+      },
+    })
+      .then(console.log);
+  };
+
+  useEffect(() => {
+    axios
+      .get(`${baseUrl}/user/getUserById`, {
+        headers: {
+          Authorization: "Bearer " + authContext.token,
+        },
+      })
+      .then((result) => {
+        if (
+          result.status !== 200 ||
+          (result.status !== 201 && result.data.isError)
+        ) {
+          authContext.logout();
+          return result.status(208).json({
+            title: "Auth Error",
+            message: "Wrong user auth!",
+          });
+        }
+        setUser(result.data.user);
+      })
+      .catch((err) => {
+        return err.status(208).json({
+          title: "Auth Error",
+          message: "Wrong user auth!",
+        });
+      });
+  }, [authContext, authContext.login]);
   return (
     <div>
       {/* <h1>vsdv</h1> */}
       <div className="transactionwrapper">
-      <div className="transaction-heading">Payment Details</div>
+        <div className="transaction-heading">Payment Details</div>
         <div className="transactioncontainer">
-
-          <div className="transaction-data"> Account No : 5355598179</div>
-          <div className="transaction-data"> IFSC Code : CBIN0283105</div>
-          <div className="transaction-data"> Bank Holder Name : SANT LONGOWAL INSTITUTE OF ENGG AND TECHNOLOGY</div>
+          <div className="transaction-data">Account No : 5355598179</div>
+          <div className="transaction-data">IFSC Code : CBIN0283105</div>
+          <div className="transaction-data">
+            Bank Holder Name : SANT LONGOWAL INSTITUTE OF ENGG AND TECHNOLOGY
+          </div>
           <div className=" inputelements transaction-data transaction-ammount">
             <div className="transaction-data transaction-data-ammount">
-              Ammount Paid :
+              Amount Paid :
             </div>
             <div>
-              <input type="text" className="transaction-ammount-input"></input>
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => {
+                  setAmount(e.target.value);
+                }}
+                className="transaction-ammount-input"
+                name="amount"
+              >
+              </input>
+            </div>
+          </div>
+          <div className=" inputelements transaction-data transaction-ammount">
+            <div className="transaction-data transaction-data-ammount">
+              Payment ID :
+            </div>
+            <div>
+              <input
+                type="text"
+                value={paymentId}
+                onChange={(e) => setPaymentId(e.target.value)}
+                className="transaction-ammount-input"
+                name="paymentID"
+              >
+              </input>
             </div>
           </div>
 
@@ -26,10 +102,19 @@ const Transaction = () => {
               Payment Screenshot :
             </div>
             <div>
-              <input className="screenshot-input" type="file" />
+              <input
+                className="screenshot-input"
+                name="paymentScreenshot"
+                type="file"
+                accept="/Image/*"
+                onChange={(e) => {
+                  console.log(e.target.files[0]);
+                  setScreenshot(e.target.files[0]);
+                }}
+              />
             </div>
           </div>
-          <div className="transaction-btn">Submit</div>
+          <div className="transaction-btn" onClick={HandleSubmit}>Submit</div>
         </div>
       </div>
     </div>
